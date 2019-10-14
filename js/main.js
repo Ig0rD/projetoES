c29cc256c73bb5aa36b264812bd583b0d64547c0
Ig0rD <34953521+Ig0rD@users.noreply.github.com>
2019-10-14 16:35:23 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,20 +4,30 @@ document.addEventListener('DOMContentLoaded', function () {
   var elems = document.querySelectorAll('.materialboxed');
   var instances = M.Materialbox.init(elems, {});
 });
-menuBtn.addEventListener('onclick', function () {
-  if (isNull(document.querySelector(".menu"))){
-    let menuDiv = document.createElement("div");
-    menuDiv.classList.add("menu");
-    let menuOptions = document.createElement("ul");
-    let content = ["Home", "Loja", "Reparos"];
-    for(let i=0;i<4;i++){
-      let li = document.createElement("li");
-      let a = document.createElement("a");
-      a.innerHTML = content[i];
-      a.classList.add("menuOption");
-      li.appendChild(a);
-    }
-  } else {
-    document.querySelector(".menu").remove();
-  }
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, {});
 });
+// menu.addEventListener('click', function () {
+//   console.log("clicado");
+//   if (document.querySelector(".menu") == null){
+//     console.log("condicao");
+//     let menuDiv = document.createElement("div");
+//     menuDiv.classList.add("menu");
+//     let menuOptions = document.createElement("ul");
+//     let content = ["Home", "Loja", "Reparos"];
+//     for(let i=0;i<3;i++){
+//       let li = document.createElement("li");
+//       let a = document.createElement("a");
+//       a.innerHTML = content[i];
+//       a.classList.add("menuOption");
+//       li.appendChild(a);
+//       menuOptions.appendChild(li);
+//     }
+//     menuDiv.appendChild(menuOptions);
+//     document.body.appendChild(menuDiv);
+//   } else {
+//     document.querySelector(".menu").remove();
+//     console.log("teste")
+//   }
+// });
