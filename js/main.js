6c57d2439763479d81bfb6e7bf644ca749ce7a5a
Ig0rD <34953521+Ig0rD@users.noreply.github.com>
2019-10-15 02:59:44 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -8,26 +8,3 @@ document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.sidenav');
   var instances = M.Sidenav.init(elems, {});
 });
-// menu.addEventListener('click', function () {
-//   console.log("clicado");
-//   if (document.querySelector(".menu") == null){
-//     console.log("condicao");
-//     let menuDiv = document.createElement("div");
-//     menuDiv.classList.add("menu");
-//     let menuOptions = document.createElement("ul");
-//     let content = ["Home", "Loja", "Reparos"];
-//     for(let i=0;i<3;i++){
-//       let li = document.createElement("li");
-//       let a = document.createElement("a");
-//       a.innerHTML = content[i];
-//       a.classList.add("menuOption");
-//       li.appendChild(a);
-//       menuOptions.appendChild(li);
-//     }
-//     menuDiv.appendChild(menuOptions);
-//     document.body.appendChild(menuDiv);
-//   } else {
-//     document.querySelector(".menu").remove();
-//     console.log("teste")
-//   }
-// });
