5de759b592df1d2b2428eae4b144af4b850aa8c7
Ig0rD <34953521+Ig0rD@users.noreply.github.com>
2019-11-18 11:55:39 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,13 +1,5 @@
 let menu = document.querySelector(".menuBtn");
 
-document.addEventListener('DOMContentLoaded', function () {
-  var elems = document.querySelectorAll('.materialboxed');
-  var instances = M.Materialbox.init(elems, {});
-});
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.sidenav');
-  var instances = M.Sidenav.init(elems, {});
-});
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.dropdown-trigger');
   var instances = M.Dropdown.init(elems, {});
@@ -15,4 +7,20 @@ document.addEventListener('DOMContentLoaded', function() {
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('select');
   var instances = M.FormSelect.init(elems, {});
+});
+$(document).ready(function () {
+  $('.slider').slider({
+    indicators: false,
+    interval: 5000,
+    height: 350
+  });
+});
+$(document).ready(function () {
+  $('.carousel').carousel();
+});
+$(document).ready(function () {
+  $('.materialboxed').materialbox();
+});
+$(document).ready(function () {
+  $('.sidenav').sidenav();
 });
