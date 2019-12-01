26e62bc07532c1714b9591c52b037cb98085695f
Ig0rD <34953521+Ig0rD@users.noreply.github.com>
2019-12-01 18:27:56 -0300

    Update item.class.php

--- php/item.class.php
+++ php/item.class.php
@@ -8,7 +8,7 @@
         private $quantidadeDisponivel;
         private $descricao;
 
-        function __construct ($foto, $codCategoria, $precoVenda, $nome, $codItem, $quantidadeDisponivel, $descricao){
+        public function __construct ($foto, $codCategoria, $precoVenda, $nome, $codItem, $quantidadeDisponivel, $descricao){
             $this->foto = $foto;
             $this->codCategoria = $codCategoria;
             $this->precoVenda = $precoVenda;
